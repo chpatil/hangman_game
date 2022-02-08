1e706ddef757e1e312a2185dcc4d360a577f2016
chipat <40916066+chpatil@users.noreply.github.com>
2022-02-08 13:42:17 +0000

    initial logic for guessing if the guessed character is correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Step 1 
-
+import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
@@ -7,3 +7,15 @@ word_list = ["aardvark", "baboon", "camel"]
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+count=random.randint(0,len(word_list)-1)
+chosen_word=word_list[count]
+chosen_word=list(chosen_word)
+guess_flag=True
+while(guess_flag):
+  guess=input("Guess a letter : ").lower()
+  for i in chosen_word:
+    if i==guess:
+      print("Right")
+    else:
+      print("Wrong")
+  guess_flag=False
